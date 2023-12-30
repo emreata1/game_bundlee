5ae2ac02fb7f953c7334729b6ad6fd199103381b
Emre Ata <emreataa69@gmail.com>
2023-12-30 20:02:12 +0300

    Theme
    tema değişimi

--- game_bundlee/lib/main.dart
+++ game_bundlee/lib/main.dart
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 214, 16, 112),
+      backgroundColor: Color.fromRGBO(22, 32, 60, 1),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
